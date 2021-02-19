dff8f8d2af47c5d2b73a531973e50c3290413167
AntonioStipic <antonio.stipic2@gmail.com>
2021-02-19 11:20:27 +0100

    feat: basic validate function to test against steps

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Academy from '@jaspero/academy';
 
 @Component({
@@ -6,18 +6,52 @@ import Academy from '@jaspero/academy';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular';
-
+export class AppComponent implements OnInit {
   constructor() {
-    console.log(Academy);
-    const a = new Academy();
-    a.addStep({name: 'test'});
-    a.addStep({name: 'test2'});
+  }
 
-    // console.log(a.steps);
-    // a.step({name: 'test'})
+  title = 'angular';
+  // @ts-ignore
+  academy: Academy;
 
-    // console.log(a.steps);
+  ngOnInit() {
+    this.academy = new Academy({
+      mount: [
+        {
+          type: 'description',
+          element: '#description'
+        }
+      ]
+    });
+
+    this.academy.addStep({
+      name: 'test',
+      description: `Click 'Validate'!`,
+      validate: () => true
+    });
+    this.academy.addStep({
+      name: 'test2',
+      description: 'Bravo! Now random chance!',
+      validate: () => Math.random() < 0.1
+    });
+    this.academy.addStep({
+      name: 'test3',
+      description: 'Woosh! That was 1 in a 10 chance...',
+      validate: () => Math.random() < 0.7
+    });
+
+    this.academy.startStep('test');
+  }
+
+  validate() {
+    if (!this.academy.currentStep) {
+      return;
+    }
+
+    if (this.academy.currentStep.valid) {
+      this.academy.nextStep();
+    } else {
+      console.log('bad');
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export interface StepOptions {
+export interface StepConfig {
     /**
      * Unique Identifier
      */
@@ -20,32 +20,63 @@ export interface StepOptions {
      * Solution which appears when requested with `showSolution`
      */
     solution?: string;
+
+    /**
+     * Validate Function to compare result
+     */
+    validate?: () => boolean;
 }
 
 export class Step {
 
-    constructor(options: StepOptions) {
-        console.log('Initialized step');
-
+    constructor(options: StepConfig) {
         this.name = options.name;
         this.start = options.start || '';
         this.description = options.description || '';
         this.solution = options.solution || '';
+        this.validate = options.validate || (() => true);
     }
 
     name: string;
     start: string;
     description: string;
     solution: string;
+    validate: () => boolean;
+
+    get valid() {
+        return this.validate();
+    }
+}
+
+export interface MountConfig {
+    type: 'description' | 'editor' | 'result';
+    element: string;
+}
+
+export interface AcademyConfig {
+    mount?: MountConfig[];
 }
 
 export class Academy {
 
-    constructor() {
+    constructor(options: AcademyConfig = {}) {
         this._steps = [];
-        console.log(document);
+
+        const mount = options.mount || [];
+        for (const item of mount) {
+            this.mount(item);
+        }
     }
 
+    private _elements: {
+        description: AcademyDescriptionElement | null,
+        editor: Element | null,
+        result: Element | null
+    } = {
+        description: null,
+        editor: null,
+        result: null
+    };
     private _steps: Step[];
 
     private _currentStep: Step | undefined;
@@ -54,7 +85,15 @@ export class Academy {
         return this._currentStep;
     }
 
-    addStep(options: StepOptions) {
+    mount(config: MountConfig) {
+        this._elements[config.type] = document.querySelector(config.element);
+
+        if (!this._elements[config.type]) {
+            throw new Error(`Could not find element '${config.element}'!`);
+        }
+    }
+
+    addStep(options: StepConfig) {
         const step = new Step(options);
 
         if (this.stepExists(options.name)) {
@@ -71,6 +110,26 @@ export class Academy {
 
     startStep(name: string) {
         this._currentStep = this._steps.find(step => step.name === name);
+
+        if (!this._currentStep) {
+            throw new Error(`Could not find step '${name}'!`);
+        }
+
+        if (this._elements.description) {
+            this._elements.description.innerHTML = this._currentStep?.description || '';
+        }
+    }
+
+    nextStep(name?: string) {
+        if (!name) {
+            let index = this._steps.findIndex(step => step.name === this._currentStep?.name) + 1;
+            if (index >= this._steps.length) {
+                index = this._steps.length - 1;
+            }
+            name = this._steps[index].name;
+        }
+
+        this.startStep(name);
     }
 
     private stepExists(name: string) {
@@ -80,10 +139,22 @@ export class Academy {
 
 export default Academy;
 
-class AcademyDescription extends HTMLElement {
-    connectedCallback() {
-        this.textContent = 'Academy Description Element';
-    }
+class AcademyDescriptionElement extends HTMLElement {
+    // connectedCallback() {
+    //     this.innerHTML = 'Academy Description Element';
+    // }
+
+    // static get observedAttributes() {
+    //     return ['content', 'data', 'data-', 'data-content'];
+    // }
+    //
+    // attributeChangedCallback(attribute: string, before: string, after: string) {
+    //     console.log('attributeChangedCallback', {attribute, before, after});
+    //
+    //     if (attribute === 'content') {
+    //         this.innerHTML = after;
+    //     }
+    // }
 }
 
-customElements.define('academy-description', AcademyDescription);
+customElements.define('academy-description', AcademyDescriptionElement);
