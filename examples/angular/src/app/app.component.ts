f0fa46cbff13a87600331ad70807c11cbc4ac739
AntonioStipic <antonio.stipic2@gmail.com>
2021-02-19 08:34:07 +0100

    chore: basic package layout with starting points. angular example

---
+++ examples/angular/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import Academy from '@jaspero/academy';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular';
+
+  constructor() {
+    console.log(Academy);
+    const a = new Academy();
+    a.addStep({name: 'test'});
+    a.addStep({name: 'test2'});
+
+    console.log(a.steps);
+    // a.step({name: 'test'})
+
+    // console.log(a.steps);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,81 @@
-console.log('123');
+export interface StepOptions {
+    /**
+     * Unique Identifier
+     */
+    name: string;
+
+    /**
+     * Starting text to appear in editor
+     */
+    start?: string;
+
+    tests?: any;
+
+    /**
+     * Description of required task
+     */
+    description?: string;
+
+    /**
+     * Solution which appears when requested with `showSolution`
+     */
+    solution?: string;
+}
+
+export class Step {
+
+    constructor(options: StepOptions) {
+        console.log('Initialized step');
+
+        this.name = options.name;
+        this.start = options.start || '';
+        this.description = options.description || '';
+        this.solution = options.solution || '';
+    }
+
+    name: string;
+    start: string;
+    description: string;
+    solution: string;
+}
+
+export class Academy {
+
+    constructor() {
+        this._steps = [];
+        this._currentStep = null;
+    }
+
+    private _steps: Step[];
+
+    private _currentStep: string | null;
+
+    get currentStep() {
+        return this._currentStep;
+    }
+
+    addStep(options: StepOptions) {
+        const step = new Step(options);
+
+        if (this.stepExists(options.name)) {
+            throw new Error(`Duplicate 'name' identifier!`);
+        }
+
+        this._steps.push(step);
+        return step;
+    }
+
+    getStep(name: string) {
+        return this._steps.find(step => step.name === name);
+    }
+
+    startStep(name: string) {
+        this._currentStep = name;
+    }
+
+    private stepExists(name: string) {
+        return this._steps.some(item => item.name === name);
+    }
+}
+
+export default Academy;
