8d1ab8007cff8e9bd43551281b1e465b01cd6fc5
AntonioStipic <antonio.stipic2@gmail.com>
2021-04-26 18:17:20 +0200

    chore(jms-docs): added plugins to markdown-it

--- examples/angular/src/app/modules/wiki/wiki.component.ts
+++ examples/angular/src/app/modules/wiki/wiki.component.ts
@@ -5,6 +5,22 @@ import { Observable } from 'rxjs';
 import { map, pluck, switchMap } from 'rxjs/operators';
 // @ts-ignore
 import md from 'markdown-it';
+// const hljs = require('highlight.js');
+
+// import * as hljs from 'highlight.js';
+
+// @ts-ignore
+import * as markdownittasklists from 'markdown-it-task-lists';
+// @ts-ignore
+import * as markdownitabbr from 'markdown-it-abbr';
+// @ts-ignore
+import * as markdownitsup from 'markdown-it-sup';
+// @ts-ignore
+import * as markdownitsub from 'markdown-it-sub';
+// @ts-ignore
+import * as markdownitmark from 'markdown-it-mark';
+// @ts-ignore
+import * as markdownitins from 'markdown-it-ins';
 
 interface Wiki {
   id: string;
@@ -39,11 +55,34 @@ export class WikiComponent implements OnInit {
           })
         )
       }),
-      map(wiki => {
+      map((wiki: Wiki) => {
         console.log({wiki});
         return {
           ...wiki,
-          content: md().render(wiki.content)
+          content: md({
+            html: true,
+            breaks: true,
+            typographer: true,
+            // highlight: function (str: any, lang: string) {
+            //   if (lang && hljs.getLanguage(lang)) {
+            //     try {
+            //       // @ts-ignore
+            //       return hljs.highlight(str, { language: lang }).value;
+            //     } catch (__) {}
+            //   }
+            //
+            //   return ''; // use external default escaping
+            // }
+          })
+            .use(markdownittasklists, {
+              label: true
+            })
+            .use(markdownitabbr)
+            .use(markdownitsup)
+            .use(markdownitsub)
+            .use(markdownitmark)
+            .use(markdownitins)
+            .render(wiki.content)
         };
       })
     );
